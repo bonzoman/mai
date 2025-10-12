dd0ba2b508e432c7bb966458831e4d0375cdc6f8
sjo <bonzoman@gmail.com>
2025-10-12 11:13:50 +0900

    Initial Commit

---
+++ maiTests/maiTests.swift
@@ -0,0 +1,17 @@
+//
+//  maiTests.swift
+//  maiTests
+//
+//  Created by 오승준 on 10/12/25.
+//
+
+import Testing
+@testable import mai
+
+struct maiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
